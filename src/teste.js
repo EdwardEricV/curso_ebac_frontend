7480e6e0dd212e53574602f22a4ae80b8d0f0a03
Edward Eric Von Osterroht <eric199703@hotmail.com>
2025-05-03 15:16:07 -0300

    Add Files

---
+++ src/teste.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudar(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicar(3, 4));
+console.log(saudar("Ana"));
